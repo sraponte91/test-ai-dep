adc3e00395f4fd05eae41cb145e98ccc715215f1
Sam Aponte <sraponte91@Sams-MBP.localdomain>
2025-04-20 19:19:09 -0400

    fisrt commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref  } from "vue";
+import startScreen from './components/startScreen.vue';
+import { GoogleGenAI } from "@google/genai";
+
+const question = ref( '' );
+
+const startQuiz = async(topic) => {
+  const ai = new GoogleGenAI({ apiKey: "AIzaSyCVBijHOwf5nnP3XV7iIa_2X-t5wK5VoyM" });
+
+  const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: `Can you give me the latest news that impact trade for ${ topic }`,
+    });
+    question.value = response.text;
+}
+</script>
+
+<template>
+  <h1>Quiz Generator</h1>
+  <div id="app">
+
+    <header>
+      <div class="container">
+
+      </div>
+    </header>
+
+    <start-screen @start-quiz="startQuiz" />
+    <p>
+      {{ question }}
+    </p>
+  </div>
+</template>
---
+++ src/components/startScreen.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue'
+  const emit = defineEmits(['start-quiz']);
+  const topic = ref( '' );
+</script>
+
+<template>
+  <section class="start-screen container">
+    <h2>Latest news that impact {{ topic }}</h2>
+    <input v-model="topic" type="text" placeholder="Enter your topic">
+    <button @click="emit('start-quiz', topic)" :disabled="!topic" >Start Quiz</button>
+  </section>
+</template>
+
